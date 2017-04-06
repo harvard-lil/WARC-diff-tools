1a395bebd956b1c58e84443f9d7dd7ae0c301ffc
anastasia <anastasia.aizman@gmail.com>
2017-04-06 12:20:12 -0400

    update - name?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname+'.md')).read()
 
 setup(
-    name = "WARC-diff-tools",
+    name = "warc_diff_tools",
     version = "0.0.1",
     author = "Anastasia Aizman",
     author_email = "anastasia.aizman@gmail.com",
