15f5678a2ac26c4943b7018fdc25a420b4340c6f
anastasia <anastasia.aizman@gmail.com>
2017-04-06 12:09:51 -0400

    update - change structure

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname+'.md')).read()
 
 setup(
-    name = "warc_diff_tools",
+    name = "WARC-diff-tools",
     version = "0.0.1",
     author = "Anastasia Aizman",
     author_email = "anastasia.aizman@gmail.com",
-    description = ("WARC diff"),
+    description = ("WARC diff tools"),
     license = "BSD",
     keywords = "WARC diff",
     url = "",
-    packages=['warc_compare', 'tests'],
+    packages=['warc_diff_tools', 'tests'],
     long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
