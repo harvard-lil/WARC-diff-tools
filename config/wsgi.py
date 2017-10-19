3e30b1faab08e185d2c293714b9297ae8ed5d746
anastasia <anastasia.aizman@gmail.com>
2017-10-19 17:01:33 -0400

    bump - wip

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,18 +1,17 @@
 import os
 
-# from django.core.wsgi import get_wsgi_application
-# from gevent.monkey import patch_all; patch_all()
-# import gevent.monkey
-# gevent.monkey.patch_all(thread=False)
-from pywb.apps.frontendapp import FrontEndApp
-# from pywb.apps.rewriterapp import RewriterApp
+from werkzeug.wsgi import DispatcherMiddleware
+from django.core.wsgi import get_wsgi_application
 
-# from pywb.warcserver.warcserver import WarcServer
+from pywb.apps.frontendapp import FrontEndApp
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# application = get_wsgi_application()
-# application = init_app(frontendapp, load_yaml=True, config_file=config_file)
-
-application = FrontEndApp(config_file='config/config.yaml', custom_config={'debug': True})
-print("DOES FILE EXIST?!", os.path.exists('config/config.yaml'))
-print('application', dir(application))
+application = DispatcherMiddleware(
+    get_wsgi_application(),
+    {
+        '/archives': FrontEndApp(
+            config_file='config/config.yaml',
+            custom_config={'debug': True}),
+    }
+)
