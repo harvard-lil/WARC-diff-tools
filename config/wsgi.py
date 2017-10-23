b6e73d91a5983ba8df02a8528ce723aa53b2bb6f
anastasia <anastasia.aizman@gmail.com>
2017-10-22 20:09:05 -0400

    add - reading from live web archive (ia) recording to personal

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,11 +7,14 @@ from pywb.apps.frontendapp import FrontEndApp
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = DispatcherMiddleware(
-    get_wsgi_application(),
-    {
-        '/archives': FrontEndApp(
-            config_file='config/config.yaml',
-            custom_config={'debug': True}),
-    }
-)
+# application = DispatcherMiddleware(
+#     get_wsgi_application(),
+#     {
+#         '/archives': FrontEndApp(
+#             config_file='config/config.yaml',
+#             custom_config={'debug': True}),
+#     }
+# )
+
+
+application = FrontEndApp(config_file='config/config.yaml', custom_config={'debug': True})
